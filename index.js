ece7c5e899baeac79715fee18a8712affedaadcb
yahall0 <72397123+yahall0@users.noreply.github.com>
2022-08-01 18:01:03 +0530

    used axios for the get request.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const getCatImgs = async() => {
+/*const getCatImgs = async() => {
     const resp = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
     const data = await resp.json();
+    const imgs = document.querySelectorAll("img");
+    let k = 0;
+    for(let img of imgs)
+    {
+        img.style.display = "none";
+        img.setAttribute("src", data[k++].url);
+        setInterval(() => {
+            img.style.display = "block";
+        }, 1500);
+    }
+}*/
+
+const getCatImgs = async() => {
+    const config = {params: {limit: 10}};
+    const resp = await axios.get("https://api.thecatapi.com/v1/images/search", config);
+    const data = resp.data;
     const imgs = document.querySelectorAll("img");
     let k = 0;
     for(let img of imgs)
